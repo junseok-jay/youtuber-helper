cbc6ffaa1922ba70d1b1f920be62af8445172b98
Jay <92563314+junseok-jay@users.noreply.github.com>
2025-11-26 23:00:59 +0900

    feat: 영상 파일 수신 기능 구현

--- back-end/src/main/java/com/example/DEVs/service/HighlightService.java
+++ back-end/src/main/java/com/example/DEVs/service/HighlightService.java
@@ -2,8 +2,27 @@ package com.example.DEVs.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 @RequiredArgsConstructor
 public class HighlightService {
+
+    // 원본 영상 저장 경로
+    private static final String VIDEO_BASE_PATH = "./videos/";
+
+    public void saveVideo(MultipartFile videoFile, String videoId) throws IOException, InterruptedException{
+        Files.createDirectories(Paths.get(VIDEO_BASE_PATH));
+
+        Path filePath = Path.of(VIDEO_BASE_PATH, videoId + ".mp4");
+
+        Files.copy(videoFile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+    }
+
 }
